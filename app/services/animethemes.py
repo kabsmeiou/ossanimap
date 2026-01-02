9043bc4d4e8e2b92f07a2c0102a9bf235f38f0ef
kabsmeiou <cvincentcabral@gmail.com>
2026-01-02 16:16:48 +0800

    add: animethemes search function:

--- app/services/animethemes.py
+++ app/services/animethemes.py
@@ -18,3 +18,13 @@ def get_anime_metadata(anime_title: str) -> Anime:
     if anime_metadata == {}:
         raise Exception("Anime metadata not found")
     return Anime(**anime_metadata)
+
+def search_anime_by_name(anime_name: str) -> list[Anime]:
+    try:
+        response = requests.get(f"{ANIMETHEMES_URL}search", params={"q": anime_name})
+        response.raise_for_status()
+    except requests.RequestException as e:
+        raise Exception(f"Error connecting to animethemes API: {str(e)}")
+    data = response.json()
+    anime_list = data["anime"] if "anime" in data else []
+    return [Anime(**anime) for anime in anime_list]
