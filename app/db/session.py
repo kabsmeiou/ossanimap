8c708f6f9c9db5c56619ea33c01956c818e4abf0
kabsmeiou <cvincentcabral@gmail.com>
2026-01-03 20:40:24 +0800

    refactored db models and created save_pack service

--- app/db/session.py
+++ app/db/session.py
@@ -1,3 +1,6 @@
 from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
